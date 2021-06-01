88f537a87ee1e211e6fee7e1304b3349940ced61
Brenno Daniel <brenno.daniel.98@gmail.com>
2021-06-01 08:10:15 -0300

    more case tests for query string

--- modulo-01/Projeto01/src/lib/queryString.spec.js
+++ modulo-01/Projeto01/src/lib/queryString.spec.js
@@ -9,6 +9,29 @@ describe('Object to query string', () => {
 
     expect(queryString(obj)).toBe('name=Brenno&profession=developer');
   });
+
+  it('should create a valid query string even when an array is passed as value', () => {
+    const obj = {
+      name: 'Brenno',
+      abilities: ['JS', 'TDD'],
+    };
+
+    expect(queryString(obj)).toBe('name=Brenno&abilities=JS,TDD');
+  });
+
+  it('throw an error when an object is passed as value', () => {
+    const obj = {
+      name: 'Brenno',
+      abilities: {
+        first: 'JS',
+        second: 'TDD',
+      },
+    };
+
+    expect(() => {
+      queryString(obj);
+    }).toThrowError();
+  });
 });
 
 // describe('Query string to object', () => {
