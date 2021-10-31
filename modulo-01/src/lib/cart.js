48b177da303052a63c9a62e17555753f87b3a079
Brenno Daniel <brenno.daniel.98@gmail.com>
2021-10-31 13:42:24 -0300

    refactoring

--- modulo-01/src/lib/cart.js
+++ modulo-01/src/lib/cart.js
@@ -4,50 +4,7 @@ import find from 'lodash/find';
 import remove from 'lodash/remove';
 // utilizaremos essa biblioteca para lidar com a falta de precisão do JS em operações financeiras, e ela também fornece alguns métodos úteis para operações básicas de matemática, dentre outros..
 import Dinero from 'dinero.js';
-
-const calculatePercentageDiscount = (amount, { condition, quantity }) => {
-  if (
-    // o ? é um recurso chamado: Optional chaining operator
-    // basicamente, se o condition existe ele lê o percentage, se não existe então para no condition
-    condition?.percentage &&
-    quantity > condition.minimum
-  ) {
-    return amount.percentage(condition.percentage);
-  }
-  return Money({ amount: 0 });
-};
-
-const calculateQuantityDiscount = (amount, { condition, quantity }) => {
-  const isEven = quantity % 2 === 0;
-
-  if (condition?.quantity && quantity > condition.quantity) {
-    return amount.percentage(isEven ? 50 : 40);
-  }
-  return Money({ amount: 0 });
-};
-
-const calculateDiscount = (amount, quantity, condition) => {
-  // se condition for um array então a lista recebe um array de condition, se não então crio novo array e passo a condition
-  const list = Array.isArray(condition) ? condition : [condition];
-
-  const [higherDiscount] = list
-    .map(cond => {
-      if (cond.percentage) {
-        return calculatePercentageDiscount(amount, {
-          condition: cond,
-          quantity,
-        }).getAmount();
-      } else if (cond.quantity) {
-        return calculateQuantityDiscount(amount, {
-          condition: cond,
-          quantity,
-        }).getAmount();
-      }
-    })
-    .sort((firstVal, secondVal) => secondVal - firstVal);
-
-  return Money({ amount: higherDiscount });
-};
+import { calculateDiscount } from './discount.utils';
 
 const Money = Dinero;
 
---
+++ modulo-01/src/lib/discount.utils.js
@@ -0,0 +1,50 @@
+import Dinero from 'dinero.js';
+
+const Money = Dinero;
+
+Money.defaultCurrency = 'BRL';
+Money.defaultPrecision = 2;
+
+const calculatePercentageDiscount = (amount, { condition, quantity }) => {
+  if (
+    // o ? é um recurso chamado: Optional chaining operator
+    // basicamente, se o condition existe ele lê o percentage, se não existe então para no condition
+    condition?.percentage &&
+    quantity > condition.minimum
+  ) {
+    return amount.percentage(condition.percentage);
+  }
+  return Money({ amount: 0 });
+};
+
+const calculateQuantityDiscount = (amount, { condition, quantity }) => {
+  const isEven = quantity % 2 === 0;
+
+  if (condition?.quantity && quantity > condition.quantity) {
+    return amount.percentage(isEven ? 50 : 40);
+  }
+  return Money({ amount: 0 });
+};
+
+export const calculateDiscount = (amount, quantity, condition) => {
+  // se condition for um array então a lista recebe um array de condition, se não então crio novo array e passo a condition
+  const list = Array.isArray(condition) ? condition : [condition];
+
+  const [higherDiscount] = list
+    .map(cond => {
+      if (cond.percentage) {
+        return calculatePercentageDiscount(amount, {
+          condition: cond,
+          quantity,
+        }).getAmount();
+      } else if (cond.quantity) {
+        return calculateQuantityDiscount(amount, {
+          condition: cond,
+          quantity,
+        }).getAmount();
+      }
+    })
+    .sort((firstVal, secondVal) => secondVal - firstVal);
+
+  return Money({ amount: higherDiscount });
+};
