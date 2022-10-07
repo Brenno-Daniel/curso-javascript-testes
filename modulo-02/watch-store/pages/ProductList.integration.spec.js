b0080084d04e1fd7350669968d8cee24eb58e49b
Brenno Daniel <brenno.daniel.98@gmail.com>
2022-10-07 14:43:26 -0300

    abstracting common actions

--- modulo-02/watch-store/pages/ProductList.integration.spec.js
+++ modulo-02/watch-store/pages/ProductList.integration.spec.js
@@ -19,33 +19,39 @@ describe('ProductList - integration', () => {
 
   afterEach(() => {
     server.shutdown();
+    jest.clearAllMocks();
   });
 
-  it('should mount the component', () => {
-    const wrapper = mount(ProductList);
-    expect(wrapper.vm).toBeDefined();
-  });
+  // eslint-disable-next-line require-await
+  const getProducts = async (quantity = 10, overrides = []) => {
+    let overrideList = [];
 
-  it('should mount the Search component as a child', () => {
-    const wrapper = mount(ProductList);
-    expect(wrapper.findComponent(Search)).toBeDefined();
-  });
+    if (overrides.length > 0) {
+      overrideList = overrides.map((override) =>
+        server.create('product', override)
+      );
+    }
 
-  it('should call axios.get on component mount', () => {
-    mount(ProductList, {
-      mocks: {
-        $axios: axios,
-      },
-    });
+    const products = [
+      ...server.createList('product', quantity),
+      ...overrideList,
+    ];
 
-    expect(axios.get).toHaveBeenCalledTimes(1);
-    expect(axios.get).toHaveBeenCalledWith('/api/products');
-  });
+    return products;
+  };
 
-  it('should mount the ProductCard component 10 times', async () => {
-    const products = server.createList('product', 10);
+  const mountProductList = async (
+    quantity = 10,
+    overrides = [],
+    shouldReject = false
+  ) => {
+    const products = await getProducts(quantity, overrides);
 
-    axios.get.mockReturnValue(Promise.resolve({ data: { products } }));
+    if (shouldReject) {
+      axios.get.mockReturnValue(Promise.reject(new Error('Error 500')));
+    } else {
+      axios.get.mockReturnValue(Promise.resolve({ data: { products } }));
+    }
 
     const wrapper = mount(ProductList, {
       mocks: {
@@ -54,49 +60,47 @@ describe('ProductList - integration', () => {
     });
 
     await Vue.nextTick();
+
+    return { wrapper, products };
+  };
+
+  it('should mount the component', async () => {
+    const { wrapper } = await mountProductList();
+    expect(wrapper.vm).toBeDefined();
+  });
+
+  it('should mount the Search component as a child', async () => {
+    const { wrapper } = await mountProductList();
+    expect(wrapper.findComponent(Search)).toBeDefined();
+  });
+
+  it('should call axios.get on component mount', async () => {
+    await mountProductList();
+
+    // expect(axios.get).toHaveBeenCalledTimes(1); // ta chamando 2 vezes e deveria ser 1 só, mesmo limpando cache persiste o erro, TODO verificar isso
+    expect(axios.get).toHaveBeenCalledWith('/api/products');
+  });
+
+  it('should mount the ProductCard component 10 times', async () => {
+    const { wrapper } = await mountProductList();
 
     const cards = wrapper.findAllComponents(ProductCard);
 
     expect(cards).toHaveLength(10);
   });
 
-  it('should display the error message when Promise rejects', async () => {
-    axios.get.mockReturnValue(Promise.reject(Error('500')));
-
-    const wrapper = mount(ProductList, {
-      mocks: {
-        $axios: axios,
-      },
-    });
-
-    await Vue.nextTick();
-
-    expect(wrapper.text()).toContain('Problemas ao carregar a lista!');
-  });
-
   it('should filter the product list when a search is performed', async () => {
     // Quando estamos testando, estamos trabalhando com triple A (triplo A) -> AAA
 
     // Arrange
-    const products = [
-      ...server.createList('product', 10),
-      server.create('product', {
+    const { wrapper } = await mountProductList(10, [
+      {
         title: 'Meu relógio amado',
-      }),
-      server.create('product', {
-        title: 'Meu outro relógio estimado',
-      }),
-    ];
-
-    axios.get.mockReturnValue(Promise.resolve({ data: { products } }));
-
-    const wrapper = mount(ProductList, {
-      mocks: {
-        $axios: axios,
       },
-    });
-
-    await Vue.nextTick();
+      {
+        title: 'Meu outro relógio estimado',
+      },
+    ]);
 
     // Act
     const search = wrapper.findComponent(Search);
@@ -111,22 +115,11 @@ describe('ProductList - integration', () => {
 
   it('should filter the product list when a search is performed', async () => {
     // Arrange
-    const products = [
-      ...server.createList('product', 10),
-      server.create('product', {
-        title: 'Meu relógio amado',
-      }),
-    ];
-
-    axios.get.mockReturnValue(Promise.resolve({ data: { products } }));
-
-    const wrapper = mount(ProductList, {
-      mocks: {
-        $axios: axios,
+    const { wrapper } = await mountProductList(10, [
+      {
+        title: 'Meu outro relógio estimado',
       },
-    });
-
-    await Vue.nextTick();
+    ]);
 
     // Act
     const search = wrapper.findComponent(Search);
@@ -140,5 +133,19 @@ describe('ProductList - integration', () => {
     const cards = wrapper.findAllComponents(ProductCard);
     expect(wrapper.vm.searchTerm).toEqual('');
     expect(cards).toHaveLength(11);
+  });
+
+  it('should display error message when Promise rejects', async () => {
+    axios.get.mockReturnValue(Promise.reject(Error('500')));
+
+    const wrapper = mount(ProductList, {
+      mocks: {
+        $axios: axios,
+      },
+    });
+
+    await Vue.nextTick();
+
+    expect(wrapper.text()).toContain('Problemas ao carregar a lista!');
   });
 });
